af45779a4f2cf5500acd85661554091bd3747eb8
nhan.trinh <hongnhan50@gmail.com>
2025-07-06 21:01:15 +0700

    feature: markdown app

--- guicmm.go
+++ guicmm.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	//"fmt"
-
 	"fyne.io/fyne/v2"
-	//"fmt"
-	//"github.com/spf13/cobra"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -15,7 +12,15 @@ type App struct {
 	output *widget.Label
 }
 
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
 var myApp App
+var cfg config
 
 func main() {
 	//var rootCmd = &cobra.Command{
@@ -25,22 +30,39 @@ func main() {
 	//		fmt.Println("Hello, Cobra CLI!")
 	//	},
 	//}
+	//a := app.New()
+	//w := a.NewWindow("Hello World")
+	//output, entry, btn := myApp.makeUI()
+	//w.SetContent(container.NewVBox(output, entry, btn))
+	//w.Resize(fyne.Size{Width: 500, Height: 500})
+	//w.ShowAndRun()
 	a := app.New()
-	w := a.NewWindow("Hello World")
-	output, entry, btn := myApp.makeUI()
-	w.SetContent(container.NewVBox(output, entry, btn))
-	w.Resize(fyne.Size{Width: 500, Height: 500})
-	w.ShowAndRun()
+	win := a.NewWindow("Mark down")
+
+	edit, preview := cfg.makeUI()
+	win.SetContent(container.NewHSplit(edit, preview))
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
 }
 
-func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
-	output := widget.NewLabel("Hello world")
-	entry := widget.NewEntry()
-	btn := widget.NewButton("Enter", func() {
-		app.output.SetText(entry.Text)
-	})
-	btn.Importance = widget.HighImportance
-	app.output = output
-
-	return output, entry, btn
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+	edit.OnChanged = preview.ParseMarkdown
+	return edit, preview
 }
+
+//func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+//	output := widget.NewLabel("Hello world")
+//	entry := widget.NewEntry()
+//	btn := widget.NewButton("Enter", func() {
+//		app.output.SetText(entry.Text)
+//	})
+//	btn.Importance = widget.HighImportance
+//	app.output = output
+//
+//	return output, entry, btn
+//}
