f82668c4602acddc4cf06fff4bc16ae540259172
nhan.trinh <hongnhan50@gmail.com>
2025-07-17 06:14:00 +0700

    feature: gold screen

---
+++ gold.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"log"
+	"os"
+)
+
+type Config struct {
+	App      fyne.App
+	InfoLog  *log.Logger
+	ErrorLog *log.Logger
+}
+
+var myApp Config
+
+func main() {
+	fyneApp := app.NewWithID("gold")
+	myApp.App = fyneApp
+	myApp.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	myApp.InfoLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	win := fyneApp.NewWindow("Gold")
+	win.ShowAndRun()
+}
