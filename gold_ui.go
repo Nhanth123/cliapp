00c33e26115632a330b36b05f4705ee5ce7ada5f
nhan.trinh <hongnhan50@gmail.com>
2025-07-29 02:59:26 +0700

    feature: get price

--- gold_ui.go
+++ gold_ui.go
@@ -1,5 +1,11 @@
-﻿package main
+package main
+
+import "fyne.io/fyne/v2/container"
 
 func (app *Config) makeUI() {
-	
+	// get current price
+	openPrice, currentPrice, priceChange := app.getPriceText()
+
+	//put infor in container
+	priceContent := container.NewGridWithColumns(3, openPrice, currentPrice, priceChange)
 }
